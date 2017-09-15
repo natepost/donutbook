88df1aa894d9fd4b39db9c8b226a4a43375ea2b4
natepost <nathanielpost@gmail.com>
2017-09-14 20:45:18 -0400

    Home and Post controller and Devise install and setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  ## Nate Post says (via devise instructions)...change host to actual host of application
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
